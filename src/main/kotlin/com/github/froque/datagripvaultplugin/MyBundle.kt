fcc0f7596b2882651a9aeb7592139bab902538dd
GitHub Action <action@github.com>
2022-01-30 17:13:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/froque/datagripvaultplugin/MyBundle.kt
+++ src/main/kotlin/com/github/froque/datagripvaultplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.froque.datagripvaultplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/froque/datagripvaultplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.froque.datagripvaultplugin.services
+
+import com.github.froque.datagripvaultplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
