0a41fa95c8d35c8a3a30f24cf1308b8d6fd49a03
Filipe Roque <froque@premium-minds.com>
2023-01-03 15:17:08 +0000

    Adds support for Vault token helper
    closes #10

---
+++ src/main/java/com/premiumminds/datagrip/vault/VaultConfig.java
@@ -0,0 +1,8 @@
+package com.premiumminds.datagrip.vault;
+
+import com.google.gson.annotations.SerializedName;
+
+public class VaultConfig {
+    @SerializedName("token_helper")
+    public String tokenHelper;
+}
