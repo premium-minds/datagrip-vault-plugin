577b12bf230a011b3568f8a93543d163c502dd53
Filipe Roque <froque@premium-minds.com>
2026-07-22 01:22:14 +0100

    Adds junit required by Intellij
    Tests fail with:
    Caused by: java.lang.ClassNotFoundException: junit.framework.TestCase

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,8 @@ dependencies {
     testImplementation(libs.jupiter)
     testImplementation(libs.postgresql)
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("junit:junit:4.13.2")
+
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
